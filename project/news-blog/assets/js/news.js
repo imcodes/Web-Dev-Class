47a33caf6dcf7a50fcc1ebed13cfcc48f9ff3b65
Ifeanyi Michael <ifmichael24@gmail.com>
2023-12-23 13:55:55 +0100

    Added modal to our News blog and Used the moment Module in our news Class

--- project/news-blog/assets/js/news.js
+++ project/news-blog/assets/js/news.js
@@ -1,3 +1,4 @@
+import moment from '../../node_modules/moment/dist/moment.js'
 export class News{
     constructor(url,apiKey,keyword){
         this.setKeyword(keyword) //set the keyword upon instantiation
@@ -85,12 +86,12 @@ export class News{
         return `<div class="col-12 col-sm-6 col-md-3">
         <div class="card border-success" data-title="${Article.title}">
             <div class="card-header p-0">
-                <a href="#">
+                <a href="javascript:void(0)">
                     <img src="${img}" class="openModal w-100" alt="">
                 </a>
             </div>
             <div class="card-body">
-                <h2 class="openModal title h6"><a title="${Article.title}">${Article.title.substr(0,40)}</a></h2>
+                <h2 class="openModal title h6"><a href="javascript:void(0)" title="${Article.title}">${Article.title.substr(0,40)}</a></h2>
             </div>
             <div class="card-footer">
                 <div class="row g-2">
@@ -109,9 +110,88 @@ export class News{
     </div>`
     }
 
-    pop(msg){
-        this.getNewsDetail(msg).then(detail => console.log(detail))
-        alert(msg)
+    showModalTemplate(Detail){
+        let {author,url,urlToImage,content,title,source,publishedAt} = Detail
+        urlToImage = urlToImage ? urlToImage : "https://placehold.co/600x400"
+        author = author ? author : 'Anonymous'
+
+        publishedAt = moment(publishedAt).toNow()
+
+        const section = document.createElement('section')
+        section.classList.add('my-modal')
+        const output = `
+        
+        <div class="my-modal-content">
+            <div class="modal-display">
+                <div class="card">
+                    <div class="card-header p-0 float-title">
+                        <img class="title-image w-100" src="${urlToImage}" alt="${title}">
+                        <h2 class="card-title px-5 py-2">${title}</h2>
+                    </div>
+                    <div class="card-body px-5 py-2">
+                        <aside class="meta-data row py-3">
+                            <div class="col-12 col-md-6" id="detail-author">
+                                <i class="fa fa-user" aria-hidden="true"></i>
+                                    <span>${author}</span>
+                            </div>
+                            <div class="col-12 col-md-6" id="detail-pub-date">
+                                <i class="fa fa-calendar-o" aria-hidden="true"></i>
+                                    <span>${publishedAt}</span>
+                            </div>
+                        </aside>
+                        <div class="py-3">
+                            <p>${content}</p>
+                        </div>
+
+                    </div>
+                    <div class="card-footer bg-white px-5 py-3">
+                        <div class="row">
+                            <div class="col-12 col-md-6" id="detail-source">News Source: <span>${source.name}</span></div>
+                            <div class="col-12 col-md-6"><a href="${url}"> Read Full content on source website &rarr;</a></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        `
+        section.innerHTML=output
+        return section
+    }
+
+    // Method that receives News Title/Caption as argument 
+    //It calls the getNewsDetail() method to fileter list of news by the recived title and return an array of on news object
+    //It then calls the showModalTemplate() method to create the template for the modal
+    //Finally it appends the Modal template to the document body
+    pop(title){
+        this.getNewsDetail(title).then(detail => {
+            // get template from the showModalTeplate method by passing the filtered data to the method
+            const mTemplate = this.showModalTemplate(detail[0]) //this method returns the template as HTML DOM
+            const bodyElement = document.querySelector('body') //get the body DOM from the document
+            
+            //listen for click event on the HTML modal template
+            //The goal is to remove the template from the document once user clicks on it.
+            mTemplate.onclick = e => {
+                //remove the clicked target from the document body
+                bodyElement.removeChild(e.target)
+            }
+
+            //add the Modal template to the document body alongside the event Listner and handler
+            bodyElement.appendChild(mTemplate)
+        })
+    }
+
+    // Method to handle the Modal open and close when some elements on the webpage are click
+    handlePop (tag) {
+        tag.querySelectorAll('[data-title]').forEach(card => {
+            const title = card.getAttribute('data-title')
+            card.querySelectorAll('.openModal').forEach( link => {
+                link.onclick = () => {
+                    this.pop(title)
+                }
+                console.log
+            })
+        })
+        
     }
 
 }
